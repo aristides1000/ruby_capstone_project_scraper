6ec10a1494de1a8088606b10cfe163efb910e62d
aristides1000 <aristides1000@gmail.com>
2021-05-06 15:37:39 -0400

    Find page indeed

--- bin/main_scraper.rb
+++ bin/main_scraper.rb
@@ -1,1 +1,8 @@
-p "hello"
+# require 'open-uri'
+# require 'net/http'
+
+url = "https://www.indeed.com/jobs?q=full%20stack%20developer&rbl=Remote&jlid=aaa2b906602aa8f5&explvl=entry_level&vjk=a62621d1694f762d"
+uri = URI.parse(url)
+response = Net::HTTP.get_response(uri)
+puts response.body
+
