2e9e735cc910676e33daead78421b8fd452ab24e
aristides1000 <aristides1000@gmail.com>
2021-05-06 21:04:21 -0400

    finish main_scraper.rb file

--- bin/main_scraper.rb
+++ bin/main_scraper.rb
@@ -1,8 +1,7 @@
-# require 'open-uri'
-# require 'net/http'
+#! /usr/bin/env ruby
 
-url = "https://www.indeed.com/jobs?q=full%20stack%20developer&rbl=Remote&jlid=aaa2b906602aa8f5&explvl=entry_level&vjk=a62621d1694f762d"
-uri = URI.parse(url)
-response = Net::HTTP.get_response(uri)
-puts response.body
+require '../lib/methods'
 
+list = Methods.new()
+
+puts list.csv_data
