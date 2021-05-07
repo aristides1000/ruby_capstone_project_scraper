339fcb7e938662a3d06bb12d6373ad2c99964a64
aristides1000 <aristides1000@gmail.com>
2021-05-06 21:11:48 -0400

    fis all rubocop errors

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -1,16 +1,16 @@
 require './lib/methods'
 
 describe Methods do
-  describe "#csv_data" do
-    it "returns a file CSV" do
-      list = Methods.new()
-      test = (list.csv_data).class
+  describe '#csv_data' do
+    it 'returns a file CSV' do
+      list = Methods.new
+      test = list.csv_data.class
       expect(test).to eql(CSV)
     end
 
-    it "the method does not return empty" do
-      list = Methods.new()
-      test = (list.csv_data).nil?
+    it 'the method does not return empty' do
+      list = Methods.new
+      test = list.csv_data.nil?
       expect(test).to eql(false)
     end
   end
